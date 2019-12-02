07bbe01a3f8fbfa5166e98292f8eeea97d56103f
unknown <dicksonwaf@gmail.com>
2019-12-02 00:52:54 -0500

    Setting up for heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT||3000
 const request = require('request')
 
 
@@ -110,7 +111,7 @@ app.get('*',(req,res)=>{
     res.send("Page not found")
 })
 
-app.listen(3000, ()=>{
-    console.log("Server up on port 3000")
+app.listen(port, ()=>{
+    console.log("Server up on port"+ port)
 })
 
